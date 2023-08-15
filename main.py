f19b0c76cb50a3ddc642af4cfb71f147c1d5e39f
Progger715 <83240866+Progger715@users.noreply.github.com>
2023-08-15 15:24:09 +0300

    init bot

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+from aiogram import Bot, Dispatcher, executor, types
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+bot = Bot(os.getenv('TOKEN'))
+dp = Dispatcher(bot=bot)
+
+
+@dp.message_handler(commands=['start'])
+async def cmd_start(message: types.Message):
+    await message.answer(f"Привет, {message.from_user.first_name}! Добро пожаловать!")
+
+
+@dp.message_handler()
+async def answer(message: types.Message):
+    await message.reply("Неизвестная команда")
+    
 
 if __name__ == '__main__':
-    pass
+    executor.start_polling(dp)
